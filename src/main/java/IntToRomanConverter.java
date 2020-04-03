a3af46b819a9d46025848bfc2d94ddb39575c84c
Sauban bin Usman <saubanbinusman@yahoo.com>
2020-04-04 02:13:13 +0500

    Refactored logic for conversion

--- src/main/java/IntToRomanConverter.java
+++ src/main/java/IntToRomanConverter.java
@@ -5,41 +5,43 @@ public class IntToRomanConverter {
     private static final Map<Integer, String> unitSymbols = new HashMap<Integer, String>() {
         {
             put(1, "I");
+            put(4, "IV");
             put(5, "V");
+            put(9, "IX");
             put(10, "X");
+            put(40, "XL");
             put(50, "L");
+            put(90, "XC");
             put(100, "C");
+            put(400, "CD");
             put(500, "D");
+            put(900, "CM");
             put(1000, "M");
         }
     };
 
     private static final List<Integer> unitSizes = Arrays.asList(
-            1000, 500, 100, 50, 10, 5, 1
+            1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
     );
 
-    private static String convertRecursive(int input) {
-        String answer = "";
+    private static String convertRecursive(int input) throws InvalidInputException {
+        if (input < 0) {
+            throw new InvalidInputException();
+        }
 
-        for (int currentUnitSizesIndex = 0; currentUnitSizesIndex < unitSizes.size(); currentUnitSizesIndex++) {
-            Integer unitSize = unitSizes.get(currentUnitSizesIndex);
+        String result = "";
 
-            while (input >= unitSize) {
-                answer += unitSymbols.get(unitSize);
-                input -= unitSize;
+        for (Integer unitSize : unitSizes) {
+            if (input >= unitSize) {
+                result = unitSymbols.get(unitSize) + convertRecursive(input - unitSize);
+                break;
             }
         }
 
-        return answer;
+        return result;
     }
 
     public static String convert(int input) throws InvalidInputException {
-        String answer = "";
-
-        while (input > 0) {
-
-        }
-        
-        return answer;
+        return convertRecursive(input);
     }
 }
