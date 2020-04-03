d5d7be1b8a1e9dc4ed6050cb8599c2fc586fd098
Saad Ismail <saad3112@gmail.com>
2020-04-04 01:49:31 +0500

    WIP: refactor convert method for generalized implementation

--- src/main/java/IntToRomanConverter.java
+++ src/main/java/IntToRomanConverter.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class IntToRomanConverter {
     private static final Map<Integer, String> intToRomanMap = new HashMap<Integer, String>() {
@@ -12,11 +11,41 @@ public class IntToRomanConverter {
         }
     };
 
-    public static String convert(int input) throws InvalidInputException {
-        if (intToRomanMap.containsKey(input)) {
-            return intToRomanMap.get(input);
-        } else {
-            throw new InvalidInputException();
+    private static final Map<Integer, String> unitSymbols = new HashMap<Integer, String>() {
+        {
+            put(1, "I");
+            put(5, "V");
+            put(10, "X");
+            put(50, "L");
+            put(100, "C");
+            put(500, "D");
+            put(1000, "M");
         }
+    };
+
+    private static final List<Integer> unitSizes = Arrays.asList(
+            1000, 500, 100, 50, 10, 5, 1
+    );
+
+    public static String convert(int input) throws InvalidInputException {
+        String answer = "";
+
+        for (int currentUnitSizesIndex = 0; currentUnitSizesIndex < unitSizes.size(); currentUnitSizesIndex++) {
+            Integer unitSize = unitSizes.get(currentUnitSizesIndex);
+
+            while (input >= unitSize) {
+                answer += unitSymbols.get(unitSize);
+                input -= unitSize;
+            }
+        }
+        
+        return answer;
+
+
+//        if (intToRomanMap.containsKey(input)) {
+//            return intToRomanMap.get(input);
+//        } else {
+//            throw new InvalidInputException();
+//        }
     }
 }
