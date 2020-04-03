f1f1dcd2d864d0874a049ae3a44ee519a9e23ad3
AmirQadir <K163641@nu.edu.pk>
2020-04-04 02:42:43 +0500

    [Refactored] Introduced Parameterized Testing

--- src/test/java/testClass.java
+++ src/test/java/testClass.java
@@ -1,71 +1,142 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.Parameter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class testClass {
+    @Parameters(name = "TC {index}: convert({0}) should return {1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, "I"},
+                {2, "II"},
+                {3, "III"},
+                {4, "IV"},
+                {5, "V"},
+                {6, "VI"},
+                {7, "VII"},
+                {8, "VIII"},
+                {9, "IX"},
+                {10, "X"},
+                {11, "XI"},
+                {12, "XII"},
+                {13, "XIII"},
+                {14, "XIV"},
+                {15, "XV"},
+                {16, "XVI"},
+                {17, "XVII"},
+                {18, "XVIII"},
+                {19, "XIX"},
+                {20, "XX"},
+                {21, "XXI"},
+                {22, "XXII"},
+                {23, "XXIII"},
+                {24, "XXIV"},
+                {25, "XXV"},
+                {26, "XXVI"},
+                {27, "XXVII"},
+                {28, "XXVIII"},
+                {29, "XXIX"},
+                {30, "XXX"},
+                {31, "XXXI"},
+                {32, "XXXII"},
+                {33, "XXXIII"},
+                {34, "XXXIV"},
+                {35, "XXXV"},
+                {36, "XXXVI"},
+                {37, "XXXVII"},
+                {38, "XXXVIII"},
+                {39, "XXXIX"},
+                {40, "XL"},
+                {41, "XLI"},
+                {42, "XLII"},
+                {43, "XLIII"},
+                {44, "XLIV"},
+                {45, "XLV"},
+                {46, "XLVI"},
+                {47, "XLVII"},
+                {48, "XLVIII"},
+                {49, "XLIX"},
+                {50, "L"},
+                {51, "LI"},
+                {52, "LII"},
+                {53, "LIII"},
+                {54, "LIV"},
+                {55, "LV"},
+                {56, "LVI"},
+                {57, "LVII"},
+                {58, "LVIII"},
+                {59, "LIX"},
+                {60, "LX"},
+                {61, "LXI"},
+                {62, "LXII"},
+                {63, "LXIII"},
+                {64, "LXIV"},
+                {65, "LXV"},
+                {66, "LXVI"},
+                {67, "LXVII"},
+                {68, "LXVIII"},
+                {69, "LXIX"},
+                {70, "LXX"},
+                {71, "LXXI"},
+                {72, "LXXII"},
+                {73, "LXXIII"},
+                {74, "LXXIV"},
+                {75, "LXXV"},
+                {76, "LXXVI"},
+                {77, "LXXVII"},
+                {78, "LXXVIII"},
+                {79, "LXXIX"},
+                {80, "LXXX"},
+                {81, "LXXXI"},
+                {82, "LXXXII"},
+                {83, "LXXXIII"},
+                {84, "LXXXIV"},
+                {85, "LXXXV"},
+                {86, "LXXXVI"},
+                {87, "LXXXVII"},
+                {88, "LXXXVIII"},
+                {89, "LXXXIX"},
+                {90, "XC"},
+                {91, "XCI"},
+                {92, "XCII"},
+                {93, "XCIII"},
+                {94, "XCIV"},
+                {95, "XCV"},
+                {96, "XCVI"},
+                {97, "XCVII"},
+                {98, "XCVIII"},
+                {99, "XCIX"},
+                {100, "C"},
+                {200, "CC"},
+                {300, "CCC"},
+                {400, "CD"},
+                {500, "D"},
+                {600, "DC"},
+                {700, "DCC"},
+                {800, "DCCC"},
+                {900, "CM"},
+                {999, "CMXCIX"},
+                {1000, "M"}
 
-    @Test(expected = InvalidInputException.class)
-    public void testNegativeInput() throws InvalidInputException {
-        int input = -1;
-        IntToRomanConverter.convert(input);
+        });
     }
 
-    @Test
-    public void test1() throws InvalidInputException {
-        int input = 1;
+    @Parameter(0)
+    public int value;
 
-        String romanEquivalent = IntToRomanConverter.convert(input);
-        String expected = "I";
-
-        Assert.assertEquals(expected, romanEquivalent);
-    }
+    @Parameter(1)
+    public String expected;
 
     @Test
-    public void test2() throws InvalidInputException {
-        int input = 2;
-
-        String romanEquivalent = IntToRomanConverter.convert(input);
-        String expected = "II";
-
-        Assert.assertEquals(expected, romanEquivalent);
-    }
-
-    @Test
-    public void test3() throws InvalidInputException {
-        int input = 3;
-
-        String romanEquivalent = IntToRomanConverter.convert(input);
-        String expected = "III";
-
-        Assert.assertEquals(expected, romanEquivalent);
-    }
-
-    @Test
-    public void test4() throws InvalidInputException {
-        int input = 4;
-
-        String romanEquivalent = IntToRomanConverter.convert(input);
-        String expected = "IV";
-
-        Assert.assertEquals(expected, romanEquivalent);
-    }
-
-    @Test
-    public void test9() throws InvalidInputException {
-        int input = 9;
-
-        String romanEquivalent = IntToRomanConverter.convert(input);
-        String expected = "IX";
-
-        Assert.assertEquals(expected, romanEquivalent);
-    }
-
-    @Test
-    public void test38() throws InvalidInputException {
-        int input = 38;
-
-        String romanEquivalent = IntToRomanConverter.convert(input);
-        String expected = "XXXVIII";
-
+    public void testInputs() throws InvalidInputException {
+        String romanEquivalent = IntToRomanConverter.convert(value);
         Assert.assertEquals(expected, romanEquivalent);
     }
 }
